7054db666986ef065ecfd1b2de1415ff84e64bfa
prakhar141 <prakhar.mathur2020@gmail.com>
2026-04-26 13:19:04 +0530

    Add: baseball-game - submission-0

---
+++ Data Structures & Algorithms/baseball-game/submission-0.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int calPoints(vector<string>& operations) {
+        vector<int> record;
+        for(const string& op:operations){
+            if(op=="C"){
+                record.pop_back();
+            }
+            else if(op=="D"){
+                record.push_back(2*record.back());
+            }
+            else if(op=="+"){
+                int n=record.size();
+                record.push_back(record[n-1]+record[n-2]);
+            }
+            else{
+                record.push_back(stoi(op));
+            }
+
+        }
+        int total=0;
+        for(int score:record){
+            total+=score;
+        }
+        return total;
+    }
+};
