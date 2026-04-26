2b8f54ba9f23d514f16d6132d8698363efd30617
prakhar141 <prakhar.mathur2020@gmail.com>
2026-04-26 17:39:33 +0530

    Add: sqrtx - submission-0

---
+++ Data Structures & Algorithms/sqrtx/submission-0.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        if(x<2) return x;
+        int low=1;
+        int high=x;
+        int ans=0;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            long long sq=(long long)mid*mid;
+            if(sq==x) return mid;
+            else if(sq<x){
+                ans=mid;
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
+        }
+        return ans;
+    }
+};
