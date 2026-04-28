0132394b61a64b599454ed023ede5334f5dbf90a
prakhar141 <prakhar.mathur2020@gmail.com>
2026-04-28 10:13:24 +0530

    Add: dota2-senate - submission-0

---
+++ Data Structures & Algorithms/dota2-senate/submission-0.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string predictPartyVictory(string senate) {
+        int n=senate.size();
+        queue<int> radiant,dire;
+        for(int i=0;i<n;i++){
+            if(senate[i]=='R') radiant.push(i);
+            else dire.push(i);
+        }
+        while(!radiant.empty() && !dire.empty()){
+            int r=radiant.front();
+            int d=dire.front();
+            radiant.pop();
+            dire.pop();
+            if(r<d) radiant.push(r+n);
+            else dire.push(d+n);
+        }
+        return radiant.empty() ? "Dire":"Radiant";
+    }
+};
